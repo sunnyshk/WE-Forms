ca0ceef78fd89699d9073a9d84d891cf01c5b4f1
Juned Shaikh <juned@Juneds-MacBook-Air.local>
2022-10-11 18:33:36 +0530

    changes

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 
+let data = JSON.parse(localStorage.getItem("userInfo")) || [];
+
 const Feedback = () => {
     const [info, setInfo] = useState({
         userId:'',
@@ -8,7 +10,7 @@ const Feedback = () => {
         lastName:''
 
     });
-    let API_KEY = '5f11970a-cd7d-4521-a978-ee147a3982d1';
+    // let API_KEY = '5f11970a-cd7d-4521-a978-ee147a3982d1';
 
     const handleChange = (e) =>{
      setInfo((prev)=>{
@@ -17,29 +19,23 @@ const Feedback = () => {
     }
     const handleSubmit = async (e) =>{
         e.preventDefault();
-        try {
-          await axios.post('https://api.webengage.com/v1/accounts/~10a5cb1dc/users',info,{
-            headers:{
-                'Access-Control-Allow-Origin': '*',
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer 5f11970a-cd7d-4521-a978-ee147a3982d1',
-            }
-           })     
-        } catch (error) {
-            console.log(error)
-        }
-        console.log(info)
+        window.webengage.user.login(info.userId)
+        localStorage.setItem('userInfo', JSON.stringify(info))
+        
     }
   return (
     <div>
         <form onSubmit={handleSubmit}>
-          <input type="text" placeholder='Enter email' name='email'  onChange={handleChange}/>
+          <input type="text" placeholder='Enter email' name='userId'  onChange={handleChange}/>
           <input type="text" placeholder='First Name' name='firstName'  onChange={handleChange}/>
           <input type="text" placeholder='Last Name' name='lastName' onChange={handleChange}/>
-          <button type='submit'>Submit</button>
+          <button type='submit' id="btnSubmit">Submit</button>
         </form>
     </div>
   )
+  
 }
+
+
 
 export default Feedback
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+let data = JSON.parse(localStorage.getItem('userInfo')) || [];
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -18,3 +21,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
